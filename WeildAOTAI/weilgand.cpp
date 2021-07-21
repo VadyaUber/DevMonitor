dce2561147d5e19a52beb797ffd333146d140abb
spaggetty <spaggetty@yandex.ru>
2021-07-21 17:25:47 +0300

    Change RFID
    Сделана проверка четного и нечетного битов, добавлена функция индикации

--- WeildAOTAI/weilgand.cpp
+++ WeildAOTAI/weilgand.cpp
@@ -23,58 +23,79 @@
 
 using  namespace std;
 
+string RFID::BitVal = "";
+int RFID::t = 15;
+int RFID::timeout = t;
 
 
-
-string  uint8_to_hex_string(uint8_t *v, const size_t s);
+//string  uint8_to_hex_string(uint8_t *v, const size_t s);
 //void * loop(void *);
-string weilgand_id="0000000000";
-bool wg_upd = false;
-string BitVal = "";
-int t = 15;
-int timeout = t;
-void getData0(void) {
+RFID::RFID()
+{
+
+}
+
+void RFID::getData0() {
 
 
 	BitVal += "1";
 	timeout = t;
 }
 
-void getData1(void) {
+void RFID::getData1() {
 
 	BitVal += "0";
 	timeout = t;
 }
 
-int wiegandInit(int d0pin, int d1pin) {
+int RFID::wiegandInit(int d0pin, int d1pin) {
 	// Setup wiringPi
 	//wiringPiSetup();
 	//pinMode(d0pin, INPUT);
 	//pinMode(d1pin, INPUT);
 	//pullUpDnControl(d0pin, PUD_UP);
 	//pullUpDnControl(d1pin, PUD_UP);
-	wiringPiISR(d0pin, INT_EDGE_FALLING, getData0);
-	wiringPiISR(d1pin, INT_EDGE_FALLING, getData1);
+	wiringPiISR(d0pin, INT_EDGE_FALLING, &getData0);
+	wiringPiISR(d1pin, INT_EDGE_FALLING, &getData1);
 
 }
 
+bool RFID::getParity(uint32_t n)
+{
+	bool parity = 0;
+	while (n) {
+		parity ^= (n & 1);
+		n >>= 1;
+	}
+	return (parity);
+}
 
 
-uint32_t CovnerterToUint() {
+uint32_t RFID::CovnerterToUint() {
 	uint32_t CardId = 0;
+	bool bit1 = BitVal[0] != '0';
+	bool bit2 = BitVal[BitVal.length() - 1] != '0';
 	BitVal = BitVal.substr(1, BitVal.length() - 2);
 	const char * charts_bit_val = BitVal.c_str();
 	for (char c : BitVal)
 		CardId = (CardId << 1) | (c - '0');
-	return CardId;
+
+	uint32_t vl1 = (CardId >> 12);
+	uint32_t vl2 = CardId & 0xFFF;
+
+	if ((getParity(vl1) == bit1) && (getParity(vl2) != bit2))
+		return CardId;
+	else
+		return 0;
 }
 
 
-void wiegand_loop(int d0pin, int d1pin, bool WG26)
+void RFID::wiegand_loop(int d0pin, int d1pin, bool WG26)
 {
+	Led_RFID = Wait;
 	int i;
-	string tmp = "";
 	wiegandInit(d0pin, d1pin);
+	string tmp = "";
 	while (1) {
 		if (BitVal != "") {
 			timeout = timeout - 1;
@@ -85,10 +106,19 @@ void wiegand_loop(int d0pin, int d1pin, bool WG26)
 				//printf("%d \r\n ", );
 				uint32_t tmp = CovnerterToUint();
 				printf("%u \r\n", tmp);
-				weilgand_id = uint8_to_hex_string((uint8_t *)&tmp, 4);
+				if (tmp == Oldval && (Led_RFID == AccesTrue))
+				{
+					Oldval = 0;
+					weilgand_id = "";
+					wg_reseted = true;
+				}
+				else
+				{
+					Oldval = tmp;
+					weilgand_id = uint8_to_hex_string((uint8_t*)&tmp, 4);
+					wg_readed = true;
+				}
 				BitVal = "";
-				//wg_upd = true;
-				wg_upd = !wg_upd;
 			}
 		}
 		else {
@@ -98,8 +128,53 @@ void wiegand_loop(int d0pin, int d1pin, bool WG26)
 
 }
 
+void RFID::set_led_state()
+{
+	if (wg_readed)
+	{
+		RFID_id = "";
+		wg_readed = false;
+		wg_upd = true;
+		Led_RFID = SendServer;
+	}
+	if (wg_upd && !(*Serv_RFID))
+	{
+		RFID_id = weilgand_id;
+		wg_sendet = true;
+		TimerRFID = new MyTime();
+		TimerRFID->IntevralSleep = 4000;
+		wg_upd = false;
+	}
+	if (wg_sendet)
+	{
+		if (TimerRFID->CheckTimeEvent())
+		{
+			if (*Serv_RFID)
+			{
+				Led_RFID = AccesTrue;
+			}
+			else
+			{
+				Led_RFID = Wait;
+			}
+			wg_sendet = false;
+		}
+		if (*Serv_RFID)
+		{
+			Led_RFID = AccesTrue;
+			wg_sendet = false;
+		}
+	}
+	if (wg_reseted)
+	{
+		RFID_id = "";
+		wg_reseted = false;
+		Led_RFID = Wait;
+	}
 
-string  uint8_to_hex_string(uint8_t *v, const size_t s) {
+}
+
+string  RFID::uint8_to_hex_string(uint8_t *v, const size_t s) {
 	stringstream ss;
 
 	ss << hex << setfill('0');
--- WeildAOTAI/weilgand.h
+++ WeildAOTAI/weilgand.h
@@ -1,13 +1,56 @@
+#pragma once
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
 #include <iostream>
 #include <string>
 #include <wiringPi.h>
+#include "MyTime.h"
+
+enum Rfid_Led_Status
+{
+	Wait,
+	SendServer,
+	AccesTrue,
+	EroorRead,
+};
+
+typedef struct {
+	bool LedGreen = false;
+	bool LedBlink = true;
+	bool LedRed = false;
+}Led_status_rfid;
+
+
 using namespace std;
-extern string weilgand_id;
-extern bool wg_upd;
-void wiegand_loop(int d0pin, int d1pin,bool WG26);
-void getData0(void);
-void getData1(void);
-string  uint8_to_hex_string(uint8_t* v, const size_t s);
+class RFID
+{
+	
+	public:
+		RFID();
+		bool RFID_Err = false;
+		bool* Serv_RFID;
+		string RFID_id = "";
+		Rfid_Led_Status Led_RFID;
+		//Led_status_rfid Led_RFID;
+		void wiegand_loop(int d0pin, int d1pin, bool WG26);
+		void set_led_state();
+	private:
+		string weilgand_id = "";
+		MyTime* TimerRFID;
+		bool wg_sendet = false;
+		bool wg_upd = false;
+		bool wg_readed = false;
+		bool wg_reseted = false;
+		static string BitVal;
+		uint32_t Oldval = 0;
+		static int t;
+		static int timeout;
+		static void getData0();
+		static void getData1();
+		string  uint8_to_hex_string(uint8_t* v, const size_t s);
+		int wiegandInit(int d0pin, int d1pin);
+		uint32_t CovnerterToUint();
+		bool getParity(uint32_t n);
+};
+
